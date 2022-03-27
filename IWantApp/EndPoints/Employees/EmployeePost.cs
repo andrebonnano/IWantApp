12e5101fca8873e7e44bdc9977a50ee2f05909bd
André Bonnano <98353333+bonnano1980@users.noreply.github.com>
2022-03-27 11:24:29 -0300

    Melhorando tratamento de erros

--- IWantApp/EndPoints/Employees/EmployeePost.cs
+++ IWantApp/EndPoints/Employees/EmployeePost.cs
@@ -1,6 +1,4 @@
-﻿using IWantApp.Domain.Products;
-using IWantApp.Infra.Data;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
 
 namespace IWantApp.EndPoints.Employees;
@@ -20,7 +18,7 @@ public class EmployeePost
         };
 
         var result = userManager.CreateAsync(user, employeeRequest.Password).Result;
-        if (!result.Succeeded){return Results.BadRequest(result.Errors.First());}
+        if (!result.Succeeded){return Results.ValidationProblem(result.Errors.ConvertToProblemDetails());}
 
         var userClaims = new List<Claim>
         {
