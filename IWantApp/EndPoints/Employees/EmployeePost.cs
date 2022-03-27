7b1cdb8de1e910896eab19cfe872211c24568253
André Bonnano <98353333+bonnano1980@users.noreply.github.com>
2022-03-27 11:14:23 -0300

    Juntando todos os Claims em uma lista

--- IWantApp/EndPoints/Employees/EmployeePost.cs
+++ IWantApp/EndPoints/Employees/EmployeePost.cs
@@ -22,10 +22,13 @@ public class EmployeePost
         var result = userManager.CreateAsync(user, employeeRequest.Password).Result;
         if (!result.Succeeded){return Results.BadRequest(result.Errors.First());}
 
-        var claimResult = userManager.AddClaimAsync(user, new Claim("EmployeeCode", employeeRequest.EmployeeCode)).Result;
-        if (!claimResult.Succeeded){return Results.BadRequest(claimResult.Errors.First());}
+        var userClaims = new List<Claim>
+        {
+            new Claim("EmployeeCode", employeeRequest.EmployeeCode),
+            new Claim("Name", employeeRequest.Name)
+        };
 
-        claimResult = userManager.AddClaimAsync(user, new Claim("Name", employeeRequest.Name)).Result;
+        var claimResult = userManager.AddClaimsAsync(user, userClaims).Result;
         if (!claimResult.Succeeded){return Results.BadRequest(claimResult.Errors.First());}
 
         return Results.Created("/employees/" + user.Id, user.Id);
