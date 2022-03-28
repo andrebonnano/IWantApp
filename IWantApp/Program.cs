3152a2e1eadfc7057ec542c8a68c7dc2927a5720
André Bonnano <98353333+bonnano1980@users.noreply.github.com>
2022-03-28 17:12:22 -0300

    Criando endopoint showcase

--- IWantApp/Program.cs
+++ IWantApp/Program.cs
@@ -80,6 +80,7 @@ app.MapMethods(EmployeeGetAll.Template, EmployeeGetAll.Methods, EmployeeGetAll.H
 
 app.MapMethods(ProductPost.Template, ProductPost.Methods, ProductPost.Handle);
 app.MapMethods(ProductGetAll.Template, ProductGetAll.Methods, ProductGetAll.Handle);
+app.MapMethods(ProductGetShowcase.Template, ProductGetShowcase.Methods, ProductGetShowcase.Handle);
 
 app.MapMethods(TokenPost.Template, TokenPost.Methods, TokenPost.Handle);
 
